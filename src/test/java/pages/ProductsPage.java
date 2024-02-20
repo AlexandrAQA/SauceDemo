2716056279fc5327323f611f2c78295272633bed
aqaacc <mralextb@gmail.com>
2024-02-20 18:33:22 +0100

    HeaderPage was added, ProductsPage and CartPage were updated, productsShouldBeAddedToCartTest has been almost completely updated

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -6,16 +6,21 @@ import org.openqa.selenium.WebElement;
 
 public class ProductsPage extends BasePage {
     private final By TITLE = By.xpath(".//span[@class='title']");
-
+    private final String addToCartButton =
+            ".//div[text()='%s']/ancestor::div[@class='inventory_item']//button";
     public ProductsPage(WebDriver driver) {
         super(driver);
     }
 
+    public void openCart() {
+        driver.get(BASE_URL + "cart.html");
+    }
     public WebElement getTitle() {
         return driver.findElement(TITLE);
     }
 
-    public void open() {
-        driver.get(BASE_URL + "inventory.html");
+    public void addProductToCart(String productName) {
+       By fullLocator = By.xpath(String.format(addToCartButton, productName));
+       driver.findElement(fullLocator).click();
     }
 }
